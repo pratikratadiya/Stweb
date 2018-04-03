bcae32ecf6371afd2057fad78ad6eceae45f77a0
Pratik <prratadiya@gmail.com>
2018-04-03 22:07:00 +0530

    Technically done

--- st_app/st_web/admin.py
+++ st_app/st_web/admin.py
@@ -1,18 +1,21 @@
+#All admin related settings and model registration is done here
+
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from st_web.models import user
-from st_web.models import foss
-from st_web.models import tutorial_detail
-from st_web.models import payment
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin   				# User admin method to be used 
+from .forms import CustomUserCreationForm, CustomUserChangeForm		#importing forms 
+from st_web.models import user,foss,tutorial_detail,payment		#importing models 
+
+# User admin class with user creation form and fields to be displayed 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = user
-    list_display = ['username', 'email', 'contributions']
+    model = user                   # User model is to be worked with
+    list_display = ['username', 'email', 'contributions']		# These fields will be displayed in admin panel under user table 
+
+
+#Registering all the models to be used for in admin panel 
 
 admin.site.register(user, CustomUserAdmin)
 admin.site.register(foss)
